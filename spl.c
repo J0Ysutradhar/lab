d22da2ab929db1f4c33fb0923fac800703558aae
Joy Sutradhar <sutradharjoy2006@gmail.com>
2026-02-15 17:44:47 +0600

    Add C program for day name and vowel checking
    This program includes three functionalities: determining the day of the week based on user input, checking if a character is a vowel, and identifying if a character is a vowel or consonant while ensuring it's a letter.

---
+++ spl.c
@@ -0,0 +1,71 @@
+// question 1: Right C program to find the day name depending on user input. User will provide any number from 1 to 7.
+
+#include <stdio.h>
+ int main (){
+
+     int day;
+     printf("Enter a number betwwen 1 to 7: ");
+     scanf("%d", &day);
+     if (day ==1){printf("Today is  Monday");}
+     if (day ==2){printf("Today is  TUE");}
+     if (day ==3){printf("Today is  Wed");}
+     if (day ==4){printf("Today is  Thu");}
+     if (day ==5){printf("Today is  Friday");}
+     if (day ==6){printf("Today is  Sat");}
+     if (day ==7){printf("Today is  Sun");}
+
+   return 0;
+ }
+
+// Uestion 2: Find whether the inputted character is a vowel or not.
+
+#include <stdio.h>
+
+int main()
+{
+    char character;
+    printf("Enter a Character: ");
+    scanf("%c", &character);
+    if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+    {
+        printf("%c is a vowel", character);
+    }
+
+    else
+    {
+        printf("%c is not a vowel", character);
+    }
+
+    return 0;
+}
+
+// Uestion 3: Find whether the inputted character is a vowel or Consonant befor you have to check it's a latter.
+
+#include <stdio.h>
+
+int main()
+{
+    char character;
+    printf("Enter a character: ");
+    scanf("%c", &character);
+
+    if (character >= 'a' && character <= 'z')
+    {
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+        {
+            printf("%c is a vowel", character);
+        }
+
+        else
+        {
+            printf("%c It is not a vowel", character);
+        }
+    }
+
+    else
+    {
+        printf("%c It is not character", character);
+    }
+
+    return 0;
+}
