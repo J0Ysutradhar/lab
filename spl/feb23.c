c4d9753ab9d04065204f32740394f29d84536d5f
J0Ysutradhar <sutradharjoy2006@gmail.com>
2026-02-23 21:15:36 +0600

    d

--- spl/feb23.c
+++ spl/feb23.c
@@ -4,10 +4,11 @@ int main(){
     printf("Enter a Character: ");
     scanf("%c", &ch);
     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
-        printf("%c is a character", ch);
+        printf("%c is a character \n", ch);
     }
     else{
-        printf("%c is not a character", ch);
+        printf("%c is not a character \n", ch);
+        
     }
 
     return 0;
